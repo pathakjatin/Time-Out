e908ed54ce5ed8246aed9294f6cac53a0cd44206
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-09-10 23:44:19 +0530

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState , useRef } from "react";
+
+export default function Player() {
+
+  const playerName = useRef();
+
+  const [enteredPlayerName , setenteredPlayerName] = useState(null);
+
+  function handleClick(){
+    setenteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome { enteredPlayerName ? enteredPlayerName : 'unknown entity' }</h2>
+      <p>
+        <input 
+          ref={playerName}
+          type="text" 
+        />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
